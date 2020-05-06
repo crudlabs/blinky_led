bd598f443e6e2ae9ca587c5773aaa91bd7b857bb
crudlabs <lab@crudlabs.org>
2020-05-06 17:21:09 -0400

    added second led

--- blinky_led.ino
+++ blinky_led.ino
@@ -1,13 +1,16 @@
 int ledPin = 12;
+int ledPin2 = 13;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
   delay(500);
-
 }
