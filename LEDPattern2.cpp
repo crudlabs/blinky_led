cb9076585585651740e96fee624c0d876b2b9105
crudlabs <lab@crudlabs.org>
2020-05-06 20:35:17 -0400

    added led pattern classes

---
+++ LEDPattern2.cpp
@@ -0,0 +1,9 @@
+#include "LEDPattern1.h"
+
+LEDPattern1::LEDPattern1() {
+  
+}
+
+void LEDPattern1::process() {
+  
+}
---
+++ LEDPattern2.h
@@ -0,0 +1,15 @@
+#ifndef LEDPattern1_h
+#define LEDPattern1_h
+
+#include "Arduino.h"
+
+class LEDPattern1
+{
+  public:
+    LEDPattern1();
+    void process();
+
+  private:
+};
+
+#endif
